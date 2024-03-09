fa2f15877182f45b0cf0222b6916ae6176209c63
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2024-03-09 05:22:04 +0300

    Update views.py

--- new/funnel/all_views/views.py
+++ new/funnel/all_views/views.py
@@ -4,21 +4,15 @@ import traceback
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views import View
 
 from log.views import send_telegram_message
 
-
-def get_form(request) -> list:
-    email = request.POST.get('email', '')
-    phone = request.POST.get('phone', '')
-    name = request.POST.get('name', '')
-    product = request.POST.get('product', '')
-
-    return [email, phone, name, product]
+from funnel.all_views.get_form import get_form
 
 
 @csrf_exempt
-def new_form(request) -> render:
+def new_form(request):
     req = get_form(request)
 
     email, phone, name, product = req
