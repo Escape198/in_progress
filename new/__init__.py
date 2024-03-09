ec14eb94235bebdaab214bd3c16ac697c8a29dbf
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2024-03-09 09:19:08 +0300

    Add files via upload

---
+++ new/__init__.py
@@ -0,0 +1,4 @@
+from new.new.celery_app import app
+
+__all__ = ('app',)
+celery = app
---
+++ new/celery_app.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+from celery import Celery
+from django.conf import settings
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new.settings')
+
+app = Celery('new')
+app.config_from_object('django.conf:settings')
+app.conf.broker_url = settings.CELERY_BROKER_URL
+app.autodiscover_tasks()
+
+
+@app.task()
+def debug_task():
+    time.sleep(5)
+    print('Hello world')
