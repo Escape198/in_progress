53ee5fc25dd755e19c0998a0745f2a1095687bf9
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2024-03-15 06:28:38 +0300

    Update views.py

--- new/applications/funnel/views.py
+++ new/applications/funnel/views.py
@@ -7,13 +7,23 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views import View
 
 from applications.log.views import send_telegram_message
-from applications.funnel.handler import handler
-from applications.funnel.models import Lead
+from applications.funnel.models.lead import Lead
 
 
 @csrf_exempt
-def new_form(request):
+def new_form(request) -> render:
     if request.method == 'POST':
-        if True:
-            lead = handler(request.POST)
-    return render(request, 'payment.html', {'lead': lead})
+        if True: # Fraud filter
+            lead = Lead()
+            lead.setattr_request(request.POST)
+
+            if not lead.check_required_fields([
+                lead.name, lead.phone, lead.email]
+            ):
+                return HttpResponse(400, status=400)
+
+            response = render(request, 'funnel/payment.html', {'lead': lead})
+    else:
+        response = render(request, 'funnel/payment.html')
+
+    return response
